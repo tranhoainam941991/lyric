00927675212e460a41443f49b6f0c6fcfbfcd64e
NAMTH12 <namth12@fsoft.com.vn>
2017-11-17 17:36:21 +0700

    search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SearchComponent } from './search/search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -23,4 +23,8 @@ export class SearchComponent implements OnInit {
       isSearchTracksHaveLyric: new FormControl()
     });
   }
+
+  handleSubmit() {
+
+  }
 }
