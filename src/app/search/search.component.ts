2721f59f8072d73ee0780251c71cca0c32c6fe66
NAMTH12 <namth12@fsoft.com.vn>
2017-11-17 15:33:01 +0700

    first commit

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  private form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = new FormGroup({
+      termTrack: new FormControl(),
+      termArtist: new FormControl(),
+      isSearchArtist: new FormControl(),
+      isSearchTracksHaveLyric: new FormControl()
+    });
+  }
+}
